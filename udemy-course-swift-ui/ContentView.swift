4cc8c1495bf049fb27209a70d70371d30a944183
Denis Tsoi <denistsoi@gmail.com>
2020-05-17 12:30:18 +0800

    ADD: customize image/ui view;

--- udemy-course-swift-ui/ContentView.swift
+++ udemy-course-swift-ui/ContentView.swift
@@ -11,12 +11,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
-        Image("gs").resizable().aspectRatio(contentMode: .fit)
-            Image("steph")
-            
+            Image("gs").resizable().aspectRatio(contentMode: .fit)
+            Image("steph").clipShape(Circle()).background(Circle().foregroundColor(.white))
+                .overlay(Circle().stroke(Color.white, lineWidth: 4)).offset(x: 0, y: -90).padding(.bottom, -90)
+                .shadow(radius: 15)
             Text("Steph Curry")
+                .font(.system(size: 40))
+                .fontWeight(.heavy)
+            
             Spacer()
-
+            
         }.edgesIgnoringSafeArea(.top)
     }
 }
