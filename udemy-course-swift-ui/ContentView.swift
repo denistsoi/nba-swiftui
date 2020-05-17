a2a39652add3a1778d06eee0be08ecfdf8cac25d
Denis Tsoi <denistsoi@gmail.com>
2020-05-17 12:16:15 +0800

    Initial Commit

---
+++ udemy-course-swift-ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  udemy-course-swift-ui
+//
+//  Created by Denis Tsoi on 17/5/2020.
+//  Copyright © 2020 Denis Tsoi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ udemy-course-swift-uiTests/udemy_course_swift_uiTests.swift
@@ -0,0 +1,34 @@
+//
+//  udemy_course_swift_uiTests.swift
+//  udemy-course-swift-uiTests
+//
+//  Created by Denis Tsoi on 17/5/2020.
+//  Copyright © 2020 Denis Tsoi. All rights reserved.
+//
+
+import XCTest
+@testable import udemy_course_swift_ui
+
+class udemy_course_swift_uiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
